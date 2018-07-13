897e017420f8d56e2dcea99bc97c7f2124e102ed
Aleksey Belchenkov <u608110@gmail.com>
2018-07-13 20:42:45 +0300

    Using the Console

---
+++ section_2/app.js
@@ -0,0 +1,14 @@
+// Log to console
+console.time('Hello');
+  console.log('Hello World!');
+  console.log(123);
+  console.log(true);
+  let greeting = 'Hello';
+  console.log(greeting);
+  console.log([1,2,3,4]);
+  console.log({a:1, b: '345'});
+  console.table({a:1, b: '345'});
+  console.error('This is some error');
+  console.warn('This is warning');
+console.timeEnd('Hello');
+console.clear();
