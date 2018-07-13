932d46240a81f1f694ea92a84de7fd25443601f6
Aleksey Belchenkov <u608110@gmail.com>
2018-07-13 21:15:54 +0300

    Variables - var, let, const

--- section_2/app.js
+++ section_2/app.js
@@ -1,4 +1,4 @@
-// Log to console
+// ================================= Log to console
 console.time('Hello');
   console.log('Hello World!');
   console.log(123);
@@ -12,3 +12,36 @@ console.time('Hello');
   console.warn('This is warning');
 console.timeEnd('Hello');
 console.clear();
+
+// ============================= VARIABLES
+// LET
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting); // Success
+
+// CONST
+// const name = 'John';
+// console.log(name);
+// name = '';
+// Have to assing a value (error)
+
+// const person = {
+//   name: 'John',
+//   age: 30
+// };
+// //person = 'Sara'; // Error
+// person.age = 22; // Success
+// console.table(person);
+
+// const numbers = [1, 2, 3];
+// numbers.push(6); // Success
+// numbers = [6]; // Error
+
+// console.log(numbers);
